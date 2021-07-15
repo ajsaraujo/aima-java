55d28a5bd760fc2a134340421b34aabc3d961ba5
Allan Juan <allanjuansil@gmail.com>
2021-07-14 21:00:16 -0300

    Escolher o caso

--- aima-core/src/main/java/aima/core/search/csp/studyplan/Main.java
+++ aima-core/src/main/java/aima/core/search/csp/studyplan/Main.java
@@ -6,6 +6,8 @@ import aima.core.search.csp.CspSolver;
 import aima.core.search.csp.FlexibleBacktrackingSolver;
 import aima.core.search.csp.studyplan.cases.Case;
 import aima.core.search.csp.studyplan.cases.FirstCase;
+import aima.core.search.csp.studyplan.cases.SecondCase;
+import aima.core.search.csp.studyplan.cases.ThirdCase;
 import aima.core.search.csp.studyplan.models.DayTime;
 import aima.core.search.csp.studyplan.models.Interval;
 import aima.core.search.csp.studyplan.models.StudyBlock;
@@ -32,6 +34,23 @@ public class Main {
     }
 
     private static Case pickCase() {
-        return new FirstCase();
+        Scanner scanner = new Scanner(System.in);
+        int answer = -1;
+
+        while (answer < 1 || answer > 3) {
+            System.out.println("Escolha um dos casos:");
+            System.out.println("1) 3 disciplinas");
+            System.out.println("2) 5 disciplinas");
+            System.out.println("3) 8 disciplinas");
+
+            answer = scanner.nextInt();
+
+            if (answer < 1 || answer > 3) {
+                System.out.println("Você deve escolher o caso 1, 2 ou 3. Por favor, tente novamente.");
+            }
+        }
+
+        Case[] cases = new Case[]{new FirstCase(), new SecondCase(), new ThirdCase()};
+        return cases[answer - 1];
     }
 }
